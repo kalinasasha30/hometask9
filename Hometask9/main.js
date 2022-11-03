311f135b083064dc7915fdd0a2f64b9c391e0332
kalinasasha30 <112653857+kalinasasha30@users.noreply.github.com>
2022-11-03 09:12:51 +0200

    Add files via upload

---
+++ Hometask9/main.js
@@ -0,0 +1,13 @@
+let div1 = document.getElementById('first_div')
+let div2 = document.getElementById('second_div')
+let div3 = document.getElementById('third_div')
+
+function fun1() {
+    div1.style.background = 'green'
+}
+function fun2() {
+    div2.style.display = 'block'
+}
+function fun3() {
+    div3.style.transform = 'rotate(90deg)'
+}
